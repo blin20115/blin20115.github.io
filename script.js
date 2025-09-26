009dd9fb1283bc858bc7810b7932134baeddab4d
blin20115 <blin20115@gmail.com>
2025-09-26 17:21:28 -0400

    Changes to Projects section

--- script.js
+++ script.js
@@ -30,19 +30,3 @@ toggleBtn.addEventListener('click', () => {
     toggleBtn.textContent = '🌙';
   }
 });
-
-const timelineItems = document.querySelectorAll('.timeline-item');
-
-const revealTimeline = () => {
-  const triggerBottom = window.innerHeight * 0.85;
-
-  timelineItems.forEach(item => {
-    const itemTop = item.getBoundingClientRect().top;
-    if (itemTop < triggerBottom) {
-      item.classList.add('visible');
-    }
-  });
-};
-
-window.addEventListener('scroll', revealTimeline);
-window.addEventListener('load', revealTimeline);
